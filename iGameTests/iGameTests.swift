b90d9b5ff3e6f8d7a895254756ef91adce3b54e4
JKevinXu <xkevinj@gmail.com>
2025-08-19 22:02:25 +0800

    Initial Commit

---
+++ iGameTests/iGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  iGameTests.swift
+//  iGameTests
+//
+//  Created by KX on 2025/8/19.
+//
+
+import Testing
+@testable import iGame
+
+struct iGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
